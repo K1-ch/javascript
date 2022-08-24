814ce0c4f6eb38cbdb5166308f46d2b623b82be0
KiranChauhan <kc7563330@gmail.com>
2022-08-24 21:55:22 +0530

    Reverse Add
    Signed-off-by: KiranChauhan <kc7563330@gmail.com>

--- NumberCalculation/Display.js
+++ NumberCalculation/Display.js
@@ -2,19 +2,42 @@ const answer = function () {
   const num = document.querySelector("input").value;
   return result(num);
 };
+
 const result = function (num) {
+  let string = "";
+  let sum = 0;
+
+  if (num < 0) {
+    string = "-";
+
+    num = Math.abs(num);
+  }
   var rem,
     rev = 0;
   while (num != 0) {
     rem = num % 10;
     rev = rev * 10 + rem;
+    sum = sum + rem;
     num = Math.floor(num / 10);
   }
-  return rev;
+
+  return [string + rev, sum];
 };
 
 document.querySelector(".btn1").addEventListener("click", function () {
-  console.log("btn is clicked");
   const val = answer();
-  document.querySelector(".output").value = val;
+  const val1 = val[0];
+  document.querySelector(".output1").value = val1;
+
+  if (val1 == document.querySelector("input").value) {
+    document.body.appendChild(document.createElement("p"));
+    document.querySelector("body p:last-child").id = "jstext";
+    document.querySelector("#jstext").innerHTML =
+      "It is a palindrome number🎉🎉";
+  }
+});
+document.querySelector(".btn2").addEventListener("click", function () {
+  const val = answer();
+  val2 = val[1];
+  document.querySelector(".output2").value = val2;
 });
